d3894fce6364002a4150757f3968fb3305d4ebef
alfie-e <116003344+alfie-3@users.noreply.github.com>
2023-08-22 15:42:41 +0100

    Some changes
    IDK

--- Chess-Challenge/src/Evil Bot/EvilBot.cs
+++ Chess-Challenge/src/Evil Bot/EvilBot.cs
@@ -4,8 +4,7 @@ using System.Numerics;
 using System.Collections.Generic;
 using System.Linq;
 
-public class EvilBot : IChessBot
-{
+public class EvilBot : IChessBot {
     //Consts.
     private const int EVALUATION_RECURSIVE_DEPTH = 3;//This is how many moves ahead the bot will think about.
                                                      //The consts after this line are values of a move based on the state of the board after that move 
@@ -37,8 +36,7 @@ public class EvilBot : IChessBot
     //Debug variables.
     private int highestValueLastTime;
 
-    public Move Think(Board board, Timer timer)
-    {
+    public Move Think(Board board, Timer timer) {
         //Cache the state of the board.
         m_board = board;
         myBotIsWhite = m_board.IsWhiteToMove;
@@ -50,30 +48,26 @@ public class EvilBot : IChessBot
         Random rng = new Random((int)System.DateTime.UtcNow.Ticks);
         Move bestMove = moves[rng.Next(moves.Length)];
         int highestValue = Evaluate(bestMove, EVALUATION_RECURSIVE_DEPTH);
-        foreach (Move move in moves)
-        {
+        foreach (Move move in moves) {
             //Evaluate the move.
             int value = Evaluate(move, EVALUATION_RECURSIVE_DEPTH);
-            if (value > highestValue)
-            {
+            if (value > highestValue) {
                 bestMove = move;
                 highestValue = value;
             }
         }
 
         //DEBUG
-        if (highestValueLastTime != highestValue)
-        {
+        if (highestValueLastTime != highestValue) {
             highestValueLastTime = highestValue;
-            //ChessChallenge.Application.ConsoleHelper.Log("HighestValue: " + highestValue.ToString());
+            ChessChallenge.Application.ConsoleHelper.Log("HighestValue: " + highestValue.ToString());
         }
 
         //Return the move to make.
         return bestMove;
     }
 
-    public int Evaluate(Move a_move, int a_deepness)
-    {
+    public int Evaluate(Move a_move, int a_deepness) {
         PieceType movePieceType = a_move.MovePieceType;
         int capturedPieceValue = pieceValues[(int)m_board.GetPiece(a_move.TargetSquare).PieceType];
         int currentDepth = a_deepness - 1;
@@ -83,15 +77,13 @@ public class EvilBot : IChessBot
         //Get the current turn's colour.
         bool currentTurnIsWhite = m_board.IsWhiteToMove;
         bool currentTurnIsMyBot = currentTurnIsWhite == myBotIsWhite;
-        if (currentTurnIsWhite)
-        {
+        if (currentTurnIsWhite) {
             //Count white pieces vs black pieces.
             //White pieces give positive value and black pieces give negative value.
             BLACK_MULTIPLIER = -1;
             WHITE_MULTIPLIER = 1;
         }
-        else
-        {
+        else {
             //Count white pieces vs black pieces.
             //White pieces give negative value and black pieces give positive value.
             BLACK_MULTIPLIER = 1;
@@ -105,140 +97,115 @@ public class EvilBot : IChessBot
         m_board.MakeMove(a_move);
 
         //Check the board for different main game states.
-        if (m_board.IsInCheckmate())
-        {
+        if (m_board.IsInCheckmate()) {
             //Always do checkmate.
             m_board.UndoMove(a_move);
             return CHECKMATE_VALUE;
         }
 
-        if (m_board.IsInCheck())
-        {
+        if (m_board.IsInCheck()) {
             moveEvaluationScore += CHECK_VALUE;
         }
 
-        if (m_board.IsDraw())
-        { //Stalemate, draw, repetition, insuffcient material etc...
+        if (m_board.IsDraw()) { //Stalemate, draw, repetition, insuffcient material etc...
             m_board.UndoMove(a_move);
             moveEvaluationScore += DRAW_VALUE;
             return moveEvaluationScore;
         }
 
-        if (a_move.IsCastles)
-        {
+        if (a_move.IsCastles) {
             moveEvaluationScore += CASTLES_VALUE;
         }
 
-        if (a_move.IsEnPassant)
-        {
+        if (a_move.IsEnPassant) {
             moveEvaluationScore += ENPASSANT_VALUE;
         }
 
-        if (a_move.IsCapture)
-        {
+        if (a_move.IsCapture) {
             moveEvaluationScore += CAPTURE_VALUE;
         }
 
         //Get the value of the whole board if the move is made.
         int valueOfBoardIfMoveIsMade = GetValueOfBoard(m_board);
         int netMoveScore = (valueOfBoardIfMoveIsMade - boardValueBeforeMove);
-        if (netMoveScore <= 0)
-        {
+        if (netMoveScore <= 0) {
             //If move does not capture a piece.
             moveEvaluationScore += NO_ENEMY_CAPTURE_VALUE; //Discourage bot from not capturing.
 
             //Then give it more incentive to move certain pieces in that case.
-            switch (movePieceType)
-            {
-                case PieceType.King:
-                    {
+            switch (movePieceType) {
+                case PieceType.King: {
                         moveEvaluationScore += KING_MOVE_SCORE_WEIGHT;
                         break;
                     }
-                case PieceType.Queen:
-                    {
+                case PieceType.Queen: {
                         moveEvaluationScore += QUEEN_MOVE_SCORE_WEIGHT;
                         break;
                     }
-                case PieceType.Rook:
-                    {
+                case PieceType.Rook: {
                         moveEvaluationScore += ROOK_MOVE_SCORE_WEIGHT;
                         break;
                     }
-                case PieceType.Bishop:
-                    {
+                case PieceType.Bishop: {
                         moveEvaluationScore += BISHOP_MOVE_SCORE_WEIGHT;
                         break;
                     }
-                case PieceType.Knight:
-                    {
+                case PieceType.Knight: {
                         moveEvaluationScore += KNIGHT_MOVE_SCORE_WEIGHT;//Initial move weight.
 
                         //Move weight to be added depedning on if the knight is close to the center.
                         int KNIGHT_MOVE_TO_CENTER_WEIGHT = 1000;
                         bool isCloseToCenter = SquareIsCloseToCenter(a_move.TargetSquare);
-                        if (isCloseToCenter)
-                        {
+                        if (isCloseToCenter) {
                             moveEvaluationScore += KNIGHT_MOVE_TO_CENTER_WEIGHT;
                         }
-                        else
-                        {
+                        else {
                             moveEvaluationScore += (-KNIGHT_MOVE_TO_CENTER_WEIGHT);
                         }
                         break;
                     }
-                case PieceType.Pawn:
-                    {
+                case PieceType.Pawn: {
                         moveEvaluationScore += PAWN_MOVE_SCORE_WEIGHT;
 
                         //Move weight to be added depending on if the pawn is close to an end rank.
                         int PAWN_MOVE_TO_END_RANK_WEIGHT = 500;
                         bool pawnIsNotOnEndRank = IsSquareAnEndRank(a_move.StartSquare);
-                        if (pawnIsNotOnEndRank)
-                        {
+                        if (pawnIsNotOnEndRank) {
                             moveEvaluationScore += PAWN_MOVE_TO_END_RANK_WEIGHT;
                         }
-                        else
-                        {
+                        else {
                             moveEvaluationScore += (-PAWN_MOVE_TO_END_RANK_WEIGHT);
                         }
                         break;
                     }
-                default:
-                    {
+                default: {
                         break;
                     }
 
             }
         }
-        else
-        {
+        else {
 
-            if (currentTurnIsMyBot)
-            {
+            if (currentTurnIsMyBot) {
                 moveEvaluationScore += ENEMY_CAPTURED_VALUE * capturedPieceValue;
             }
-            else
-            {
+            else {
                 //If the current turn isn't my bot I want the enemy captured value to be higher so mybot is discourage from making the move that allowed that to happen.
                 moveEvaluationScore += (ENEMY_CAPTURED_VALUE * capturedPieceValue * 10);
             }
 
         }
 
-        if (currentDepth > 0)
-        {
+        if (currentDepth > 0) {
             //Get list of next posisble moves.
             Move[] nextMoves = m_board.GetLegalMoves();
 
             //Evaluate each of those moves with 1 less depth than the previous call of evaluate.
             //When it reaches 0 then the recursive loop will exit with the best approximate move.
             int worstScoreForPreviousPlayer = int.MaxValue;
-            foreach (Move move in nextMoves)
-            {
+            foreach (Move move in nextMoves) {
                 int moveScore = -Evaluate(move, currentDepth); //Inverts the evaluation score as what's best for the next player won't be best for the current player.
-                if (moveScore < worstScoreForPreviousPlayer)
-                {
+                if (moveScore < worstScoreForPreviousPlayer) {
                     worstScoreForPreviousPlayer = moveScore;
                 }
             }
@@ -255,40 +222,32 @@ public class EvilBot : IChessBot
         return moveEvaluationScore;
     }
 
-    private int GetValueOfBoard(Board a_board)
-    {
+    private int GetValueOfBoard(Board a_board) {
         PieceList[] allPieces = m_board.GetAllPieceLists();
         int boardValue = 0;
-        foreach (PieceList pieces in allPieces)
-        {
+        foreach (PieceList pieces in allPieces) {
             bool isWhitePieceList = pieces.IsWhitePieceList;
             int piecesCountTimesPieceValue = pieces.Count * pieceValues[(int)pieces.TypeOfPieceInList];
-            if (isWhitePieceList)
-            {
+            if (isWhitePieceList) {
                 boardValue += (WHITE_MULTIPLIER * piecesCountTimesPieceValue);
             }
-            else
-            {
+            else {
                 boardValue += (BLACK_MULTIPLIER * piecesCountTimesPieceValue);
             }
         }
         return boardValue;
     }
 
-    private bool SquareIsCloseToCenter(Square a_square)
-    {
-        if (a_square.File > 1 && a_square.File < 6 && a_square.Rank > 1 && a_square.Rank < 6)
-        {
+    private bool SquareIsCloseToCenter(Square a_square) {
+        if (a_square.File > 1 && a_square.File < 6 && a_square.Rank > 1 && a_square.Rank < 6) {
             return true; //Square is close to the center of the board.
         }
 
         return false;//Square is not close to the center of the board.
     }
 
-    private bool IsSquareAnEndRank(Square a_square)
-    {
-        if (a_square.Rank <= 0 || a_square.Rank >= 7)
-        {
+    private bool IsSquareAnEndRank(Square a_square) {
+        if (a_square.Rank <= 0 || a_square.Rank >= 7) {
             return true;//Square is an end rank.
         }
 
--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,31 +1,26 @@
 ﻿using ChessChallenge.API;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 
-public class MyBot : IChessBot
-{
+public class MyBot : IChessBot {
     //Value of pieces for taking and protecting - None, Pawn, Knight, Bishop, Rook, King, Queen.
-    static public int[] pieceValues = { 0, 110, 150, 200, 200, 400, 800 };
+    static public int[] pieceValues = { 0, 110, 150, 275, 275, 400, 1500 };
 
     //Cost of moving a piece, to discourage moving high value pieces - None, Pawn, Knight, Bishop, Rook, King, Queen.
-    static public int[] pieceMoveCost = { 0, 25, 38, 50, 50, 125, 175 };
+    static public int[] pieceMoveCost = { 0, 25, 38, 50, 50, 200, 150 };
 
     //Interest level of a move adds additional levels of recursion to a move
-    public enum Interest
-    {
+    public enum Interest {
         NONE = 0,
         LOW = 0,
-        MEDIUM = 1,
+        MEDIUM = 0,
         HIGH = 2,
     }
 
     //Scores awarded for various states of the game.
-    const int checkValue = 100;
+    const int checkValue = 150;
     const int checkMateValue = 1000000;
-    const int potentialCheckmateValue = 400;
-    const int drawValue = -10000000;
+    const int potentialCheckmateValue = 300;
+    const int drawValue = -100000000;
 
     //Bonuses given to special moves.
     const int promotionBonus = 100;
@@ -40,16 +35,18 @@ public class MyBot : IChessBot
     //If the turn is white, so it can be checked if the player is black or white
     static bool isWhite;
     //Multiplier that sets preference to protecting pieces on the players turn.
-    const float myPieceMultiplier = 1.4f;
+    const float myPieceMultiplier = 1.3f;
 
     //Clamps the leverage within a certain range so the results are not as extreme
-    const float leverageClamp = 1.5f;
+    const float leverageClamp = 1.25f;
     //Bonus applied for achieving a higher leverage in a move (Should be multiplied by leverage)
-    const int leverageBonus = 125;
+    const int leverageBonus = 100;
+
+    //How low a score can be before being discounted
+    const int scoreFloor = -1000;
 
     //An evaluated move with a score, a level of interest and an assigned move
-    public struct EvaluatedMove
-    {
+    public struct EvaluatedMove {
         //Score - How valuable this move is to the current player.
         public int score;
         //Intrest - How intresting a move is which can be used as a bonus to provide deeper levels of recursion.
@@ -57,22 +54,20 @@ public class MyBot : IChessBot
         //Move - The move that is made using the chess challenge API.
         public Move move;
 
-        public EvaluatedMove(Move _move)
-        {
+        public EvaluatedMove(Move _move) {
             score = 0;
             interest = Interest.NONE;
             move = _move;
         }
     }
 
-    public Move Think(Board board, Timer timer)
-    {
-        //Upon begining to think it should be the players turn.
+    public Move Think(Board board, Timer timer) {
+        //Upon begining to think it should be the players turn.d
         isMyTurn = true;
         isWhite = board.IsWhiteToMove;
 
         //Get All Moves
-        Span<Move> moveSpan = stackalloc Move[256];
+        Span<Move> moveSpan = stackalloc Move[500];
         board.GetLegalMovesNonAlloc(ref moveSpan);
 
         Span<EvaluatedMove> evaluatedMovesSpan = stackalloc EvaluatedMove[moveSpan.Length];
@@ -85,33 +80,27 @@ public class MyBot : IChessBot
         return bestMove.move;
     }
 
-    public Span<EvaluatedMove> EvaluateMoves(Span<Move> moveSpan, Board board, Timer timer, Span<EvaluatedMove> evaluatedMovesSpan, int currentDepth = 1, int recursiveDepth = baseMoveDepth)
-    {
+    public Span<EvaluatedMove> EvaluateMoves(Span<Move> moveSpan, Board board, Timer timer, Span<EvaluatedMove> evaluatedMovesSpan, int currentDepth = 1, int recursiveDepth = baseMoveDepth) {
         //Consider all legal moves, with current and max depth set, by default this is base depth
-        for (int i = 0; i < evaluatedMovesSpan.Length; i++)
-        {
+        for (int i = 0; i < evaluatedMovesSpan.Length; i++) {
             evaluatedMovesSpan[i] = EvaluateMove(moveSpan[i], board, timer, currentDepth, recursiveDepth);
         }
 
         return evaluatedMovesSpan;
     }
 
-    public static EvaluatedMove GetBestMove(Span<EvaluatedMove> evaluatedMoves)
-    {
+    public static EvaluatedMove GetBestMove(Span<EvaluatedMove> evaluatedMoves) {
         //Chooses a random move, if no move is deemed better, this is the move that is made.
         Random rng = new();
         EvaluatedMove bestMove = evaluatedMoves[rng.Next(evaluatedMoves.Length)];
 
         //Gets the best move
-        for (int i = 0; i < evaluatedMoves.Length; i++)
-        {
-            if (isMyTurn)
-            {
+        for (int i = 0; i < evaluatedMoves.Length; i++) {
+            if (isMyTurn) {
                 if (evaluatedMoves[i].score > bestMove.score)
                     bestMove = evaluatedMoves[i];
             }
-            else
-            {
+            else {
                 if (evaluatedMoves[i].score < bestMove.score)
                     bestMove = evaluatedMoves[i];
             }
@@ -120,45 +109,67 @@ public class MyBot : IChessBot
         return bestMove;
     }
 
-    public EvaluatedMove EvaluateMove(Move move, Board board, Timer timer, int currentDepth, int recursiveDepth = baseMoveDepth)
-    {
+    public EvaluatedMove EvaluateMove(Move move, Board board, Timer timer, int currentDepth, int recursiveDepth = baseMoveDepth) {
         EvaluatedMove evalMove = new(move);
         float score = 0;
 
-        //If the move is a capture move, how valuable would the captured piece be?
-        if (move.IsCapture)
-        {
-           score += EvaluatePieceValue(pieceValues[(int)move.CapturePieceType], board);
+        float evalStartLeverage = 0;
+        float evalEndLeverage = 0;
 
-            if (move.CapturePieceType == PieceType.Queen)
-                evalMove.interest = Interest.MEDIUM;
+        //===============================================
+        //Compares the board leverage at the start of the evaluation vs the end and applies a bonus if its above.
+        if (currentDepth == 1)
+            evalStartLeverage = CalculateLeverage(board);
+        else if (currentDepth == recursiveDepth)
+            evalEndLeverage = CalculateLeverage(board);
+
+        if (evalStartLeverage > evalEndLeverage) 
+            score += leverageBonus;
+        //===============================================
+
+        //If the move is a capture move, how valuable would the captured piece be?
+        if (move.IsCapture) {
+            score += EvaluatePieceValue(pieceValues[(int)move.CapturePieceType]);
         }
 
         //Each piece has a movement cost, to discourage throwing valuable pieces at the enemy
         score -= pieceMoveCost[(int)move.MovePieceType];
+
+        if (board.IsInCheck())
+            evalMove.interest = Interest.MEDIUM;
 
         //================================================
         //Checks next move to see if its advantagous
         board.MakeMove(move);
         isMyTurn = !isMyTurn;
 
-        if (board.IsInCheck())
-        {
+        if (board.IsInCheck()) {
             evalMove.interest = Interest.HIGH;
             score += checkValue;
         }
 
-        if (board.IsInCheckmate())
-        {
-            if (currentDepth == 1)
-                score += checkMateValue;
-            else
+        if (board.IsInCheckmate()) {
+            if (currentDepth == 1) {
+                evalMove.score = checkMateValue;
+
+                isMyTurn = !isMyTurn;
+                board.UndoMove(move);
+                return evalMove;
+            }
+            else if (isMyTurn) {
+                score += potentialCheckmateValue / currentDepth;
+            }
+            else {
                 score += potentialCheckmateValue;
+            }
         }
 
-        if (board.IsDraw())
-        {
-            score += drawValue;
+        if (board.IsDraw()) {
+            evalMove.score = drawValue;
+
+            isMyTurn = !isMyTurn;
+            board.UndoMove(move);
+            return evalMove;
         }
 
         isMyTurn = !isMyTurn;
@@ -178,24 +189,20 @@ public class MyBot : IChessBot
         //===============================================
 
         //Moves that are made from the other player are negative, and are retracted from a moves scoring
-        //Moves that are made from the other player are negative, and are retracted from a moves scoring
-        evalMove.score /= currentDepth;
         evalMove.score += (int)(score * TurnMultipler);
 
         //Bonus depth is added depending on how interesting that move was + time must be above 5 seconds to prevent timeout
         int depthToExplore = currentDepth == 1 && timer.MillisecondsRemaining > 5000 ? recursiveDepth + (int)evalMove.interest : recursiveDepth;
 
         //THE SECRET SAUCE // Moves are checked recursively to decide whether a move would be good or not, the moves alternate players
-        if (currentDepth < recursiveDepth)
-        {
+        if (currentDepth < recursiveDepth) {
             board.MakeMove(move);
             isMyTurn = !isMyTurn;
 
-            Span<Move> moveSpan = stackalloc Move[256];
+            Span<Move> moveSpan = stackalloc Move[500];
             board.GetLegalMovesNonAlloc(ref moveSpan);
 
-            if (moveSpan.Length > 0)
-            {
+            if (moveSpan.Length > 0) {
                 Span<EvaluatedMove> evaluatedMovesSpan = stackalloc EvaluatedMove[moveSpan.Length];
 
                 EvaluatedMove nextBestMove = GetBestMove(EvaluateMoves(moveSpan, board, timer, evaluatedMovesSpan, currentDepth + 1, depthToExplore));
@@ -213,31 +220,26 @@ public class MyBot : IChessBot
     static float TurnMultipler => isMyTurn ? 1f : -1f;
 
     //Multplier that prioritises protecting high value pieces over taking pieces.
-    static int EvaluatePieceValue(int pieceValue, Board board)
-    {
+    static float EvaluatePieceValue(int pieceValue) {
         if (isMyTurn)
-            return (int)(pieceValue);
+            return pieceValue;
         else
-            return (int)(myPieceMultiplier * pieceValue);
+            return (myPieceMultiplier * pieceValue);
     }
 
-    static float CalculateLeverage(Board board)
-    {
+    static float CalculateLeverage(Board board) {
         PieceList[] pieceLists = board.GetAllPieceLists();
         Span<PieceList> pieceListSpan = pieceLists;
 
         int whitePiecesValue = 1;
         int blackPiecesValue = 1;
 
-        foreach (PieceList pieceList in pieceListSpan)
-        {
-            if (pieceList.IsWhitePieceList)
-            {
+        foreach (PieceList pieceList in pieceListSpan) {
+            if (pieceList.IsWhitePieceList) {
                 whitePiecesValue += CalculatePieceListValue(pieceList);
                 continue;
             }
-            else
-            {
+            else {
                 blackPiecesValue += CalculatePieceListValue(pieceList);
                 continue;
             }
@@ -254,12 +256,10 @@ public class MyBot : IChessBot
         return leverage;
     }
 
-    static int CalculatePieceListValue(PieceList pieceList)
-    {
+    static int CalculatePieceListValue(PieceList pieceList) {
         int pieceListValue = 1;
-        
-        for (int i = 0; i < pieceList.Count; i++)
-        {
+
+        for (int i = 0; i < pieceList.Count; i++) {
             pieceListValue += pieceValues[(int)pieceList.GetPiece(i).PieceType];
         }
 
